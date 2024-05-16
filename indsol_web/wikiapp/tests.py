6fbb6e8c6be71c96a4629363230c667e8145b7eb
KorepanovA <al1working@MAIL.RU>
2024-05-16 15:15:48 +0300

    Добавление тестирование метода READ для эндпойнта Wiki

--- indsol_web/wikiapp/tests.py
+++ indsol_web/wikiapp/tests.py
@@ -22,10 +22,42 @@ class APIWikiTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
         self.url = "/api/v1/wiki/list/"
 
-    # CREATE METHOD READ
+    # CREATE METHOD TEST
     def test_wiki_create(self):
         body = {"name": "test2"}
         response = self.client.post(self.url, body, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Wiki.objects.count(), 1)
         self.assertEqual(Wiki.objects.get().name, "test2")
+
+    # READ METHOD TEST
+    def test_wiki_read(self):
+        # Создаем первую запись
+        body = {
+            "name": "test_1",
+        }
+        post_response = self.client.post(self.url, body, format="json")
+        id_1 = json.loads(post_response.content)["id"]
+
+        # Получаем список новостей
+        get_response = self.client.get(self.url, {}, format="json")
+
+        # Проверям полученную новость
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Wiki.objects.get(id=id_1).name, "test_1")
+        self.assertEqual(Wiki.objects.count(), 1)
+
+        # Создаем вторую запись
+        body = {
+            "name": "test_2",
+        }
+        post_response = self.client.post(self.url, body, format="json")
+        id_2 = json.loads(post_response.content)["id"]
+
+        # Получаем список новостей
+        get_response = self.client.get(self.url, {}, format="json")
+
+        # Проверям обе полученные новости
+        self.assertEqual(Wiki.objects.count(), 2)
+        self.assertEqual(Wiki.objects.get(id=id_1).name, "test_1")
+        self.assertEqual(Wiki.objects.get(id=id_2).name, "test_2")
