22f0b9ea98a15ef8084cb36dc82b7ee2c5089106
KorepanovA <al1working@MAIL.RU>
2024-06-07 09:15:20 +0300

    Созданы фикстуры для заполнения отраслей и фед округов

--- indsol_web/authapp/fixtures/regions.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open(".\\indsol_web\\authapp\data\\region.csv", "r", newline="") as file:
-    spamreader = csv.reader(file, delimiter=" ", quotechar=",")
-    for row in spamreader:
-        print(", ".join(row))
