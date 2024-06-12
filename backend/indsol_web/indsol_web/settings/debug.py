602a616b78d645086993077eac65f502a33c4a65
KorepanovA <al1working@MAIL.RU>
2024-06-12 08:59:28 +0300

    Сохранение общей статики Django

--- backend/indsol_web/indsol_web/settings/debug.py
+++ backend/indsol_web/indsol_web/settings/debug.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = True
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Время жизни токенов JWT
 SIMPLE_JWT = {
