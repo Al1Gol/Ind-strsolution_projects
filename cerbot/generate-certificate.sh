5543436350bc6734ca56a7924c8d39eeee809316
Korepanov A <al1working@mail.ru>
2024-06-19 16:27:13 +0300

    Изменен прицип получения сертификата на bash скрипт

---
+++ cerbot/generate-certificate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# выдаем себе сертификат (обратите внимание на переменные среды)
+certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
+
+# удаляем старые сертификаты из примонтированной
+# через Docker Compose папки Nginx
+rm -rf /etc/nginx/cert.pem
+rm -rf /etc/nginx/key.pem
+
+# копируем сертификаты из образа certbot в папку Nginx
+cp /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
+cp /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
