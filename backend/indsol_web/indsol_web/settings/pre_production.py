89f054c6c0475cc6152afd3cc06571390b43901b
KorepanovA <al1working@mail.ru>
2024-06-27 15:49:53 +0300

    Добавлены и закомментированы адреса CORS в pre_production для тестирования и отладки

--- backend/indsol_web/indsol_web/settings/pre_production.py
+++ backend/indsol_web/indsol_web/settings/pre_production.py
@@ -4,7 +4,18 @@ from .base import *
 
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['http://178.237.190.38:8000', 'htpp://178.34.150.253:8000']
+CSRF_TRUSTED_ORIGINS = ['http://5.182.4.206:8000', 'http://localhost:3000', 'http://178.237.190.38:8000', 'htpp://178.34.150.253:8000', 'http://194.8.128.75', 'http://194.8.12.75:8000']
+
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:3000",
+#    "http://192.168.10.107:3000",
+#    "http://192.168.10.109:3000",
+#    "http://192.168.10.238:5554",
+#    "http://192.168.10.172:3000",
+#    "http://192.168.10.210:3000",
+#    "http://192.168.10.222:3000",
+#    "http://192.168.24.82:5554",
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
