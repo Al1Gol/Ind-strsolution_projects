399d57419c04cb193ecae9e463eb603a987a953e
KorepanovA <al1working@MAIL.RU>
2024-07-10 14:23:33 +0300

    Созданы файлы миграции, исправлены опечатки в параметрах моделей

--- backend/indsol_web/newsapp/models.py
+++ backend/indsol_web/newsapp/models.py
@@ -7,9 +7,10 @@ from newsapp.validators import validate_media_extension
 class News(models.Model):
     title = models.CharField(verbose_name="Заголовок", max_length=100)
     text = models.TextField(verbose_name="Текст новости", max_length=40000)
-    to_slider = models.BooleanField(verbone="Добавить в миниленту", default=False)
-    cover = models.ImageField(verbose_name="обложка", 
-        upload_to="news/covers/", 
+    to_slider = models.BooleanField(verbose_name="Добавить в миниленту", default=False)
+    cover = models.ImageField(
+        verbose_name="обложка",
+        upload_to="news/covers/",
         validators=[validate_media_extension],
     )
     created_at = models.DateTimeField(verbose_name="дата создания", auto_now_add=True)
