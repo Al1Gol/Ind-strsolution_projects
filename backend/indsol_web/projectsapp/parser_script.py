bebf9b3f03ef2f05cefbfedc42afa2d1ee990480
Korepanov A <al1working@mail.ru>
2024-08-23 00:51:36 +0300

    Доработан скрипт выгрузки из 1С - Добавлен вывод полей "Ответственный" и "ОтветственныйРП" - Добавлена автоматическая очитска таблицы для избежания формирования дубликатов - Добавлен учет операционной системы при миграции проекта

--- backend/indsol_web/projectsapp/parser_script.py
+++ backend/indsol_web/projectsapp/parser_script.py
@@ -2,7 +2,7 @@ import json
 import psycopg2
 import os
 
-def get_dict(id, contract, todo):
+def get_dict(id, contract, todo, resp, resp_rp):
     dict_test = {
         "model": "projectsapp.projects",
         #"pk": id,
@@ -13,8 +13,8 @@ def get_dict(id, contract, todo):
             "deadline": todo['СрокВыполнения'],
             "is_completed": todo['Выполнено'],
             "actual_date": todo['ДатаФактическогоВыполнения'],
-            "responsible": None,
-            "responsible_rp": None
+            "responsible": resp,
+            "responsible_rp": resp_rp
         }
     }
     return dict_test
@@ -25,7 +25,8 @@ def unpack(projects):
 conn = psycopg2.connect(dbname='indsol_test', user='postgres', 
                         password='123', host='localhost')
 cursor = conn.cursor()
-
+cursor.execute('DELETE FROM projectsapp_projects')
+conn.commit()
 cursor.execute('SELECT contract_number FROM projectsapp_contracts')
 records = [el[0] for el in cursor.fetchall()]
 
@@ -35,21 +36,22 @@ fixture = []
 # Исходный файл состоит из списка словарей
 with open('./backend/indsol_web/projectsapp/data/111.json', mode='r', encoding='utf-8-sig') as file:
     projects_dict = json.loads(file.read())
-
     # Проходим по списку договоров
     for project in projects_dict:
         contract = project['НомерДокумента']
         todoes = project['СписокРаботПоПроекту']
+        resp = project['Ответственный']
+        resp_rp = project['ОтветственныйРП']
         if contract in records:
             cursor.execute('SELECT id FROM projectsapp_contracts where contract_number=%s', (contract, ))
             id = cursor.fetchone()[0]
-            print(id)
             for todo in todoes:
-                fixture.append(get_dict(id, contract, todo))
+                fixture.append(get_dict(id, contract, todo, resp, resp_rp))
 
 # Сохранение фикстуры
 with open('./backend/indsol_web/projectsapp/fixtures/projects.json', mode='w+', encoding='utf-8') as file:
     json.dump(fixture, file, ensure_ascii=False, indent=4)
 
 # Загрузка полученной фикстуры в приложение
-os.system('python -Xutf8 ./backend/indsol_web/manage.py loaddata projects.json --settings=indsol_web.settings.debug')
+if os.name == 'nt':
+    os.system('python -Xutf8 ./backend/indsol_web/manage.py loaddata projects.json --settings=indsol_web.settings.debug')
