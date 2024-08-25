1e57b6208bdb6a21e899f1ebef53552fac3cf054
Korepanov A <al1working@mail.ru>
2024-08-25 17:00:33 +0300

    Актуализирован файл requirements

--- backend/indsol_web/indsol_web/settings_celery.py
+++ backend/indsol_web/indsol_web/settings_celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indsol_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indsol_web.settings.pre_production')
 
 app = Celery('indsol_web')
 
@@ -11,7 +11,7 @@ app = Celery('indsol_web')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings.pre_production', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
