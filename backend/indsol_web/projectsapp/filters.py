91766f5a6cc3acd5dedcf5dfa5245e886ceffc49
Korepanov A <al1working@mail.ru>
2024-08-27 20:43:02 +0300

    Удалено лишнее поле в AdjustFilter, изменен тип данных фильтрации по client_id в Contracts_filter

--- backend/indsol_web/projectsapp/filters.py
+++ backend/indsol_web/projectsapp/filters.py
@@ -2,13 +2,11 @@ from django_filters import rest_framework as filters
 from projectsapp.models import Contracts, Adjust, Projects
 
 class ContractsFilter(filters.FilterSet):
-        client_id = filters.DateFilter("client_id__id")
+        #client_id = filters.BaseInFilter("client_id__id")
         model = Projects
         fields = ["client_id"]
 
 class AdjustFilter(filters.FilterSet):
-    created_at = filters.DateFilter("created_at__date")
-
     class Meta:
         model = Adjust
         fields = ("contract_id",)
