62deea7f0ade22232f2a8c71740a32b7a00ed349
Korepanov A <al1working@mail.ru>
2024-08-29 02:13:08 +0300

    Исправлена фильтрация для Договоров по client_id

--- backend/indsol_web/projectsapp/filters.py
+++ backend/indsol_web/projectsapp/filters.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as filters
 from projectsapp.models import Contracts, Adjust, Projects
 
 class ContractsFilter(filters.FilterSet):
-    class Meta:
+        client_id = filters.BaseInFilter("client_id__id")
         model = Projects
         fields = ["client_id"]
 
