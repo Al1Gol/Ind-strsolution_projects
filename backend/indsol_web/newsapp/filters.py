9d8d3e82d93060c8f60e8d35e8fd900508b4a88b
Korepanov A <al1working@mail.ru>
2024-09-06 21:45:15 +0300

    Добавлена фильтрация по publicated_at для News

--- backend/indsol_web/newsapp/filters.py
+++ backend/indsol_web/newsapp/filters.py
@@ -4,10 +4,11 @@ from newsapp.models import News, Media
 
 class NewsDateFilter(filters.FilterSet):
     created_at = filters.CharFilter("created_at__date", lookup_expr='icontains')
+    publicated_at = filters.CharFilter("publicated_at__date", lookup_expr='icontains')
     title = filters.CharFilter("title", lookup_expr='icontains')
 
     model = News
-    fields = ["created_at", "title",]
+    fields = ["created_at", "publicated_at", "title",]
 
 
 class MediaFilter(filters.FilterSet):
