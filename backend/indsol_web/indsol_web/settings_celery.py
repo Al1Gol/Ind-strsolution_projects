5717b49f911d10eef9b2907d11619042c6c4cef8
Korepanov A <al1working@mail.ru>
2025-01-14 01:25:45 +0300

    Добавлен task для автозагрузки выгрузок в таблицу Согласований

--- backend/indsol_web/indsol_web/settings_celery.py
+++ backend/indsol_web/indsol_web/settings_celery.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 from django.conf import settings
 from celery import Celery
 from celery import shared_task
-from projectsapp.parsers import LoadProjects
+from projectsapp.parsers import LoadProjects, LoadAdjustes
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indsol_web.settings.pre_production')
@@ -35,13 +35,36 @@ def parser_projects_task(self):
     # Настройка файлов
     # Файл выгрузки
     projects.export_path = f'{settings.BASE_DIR}/projectsapp/data/'
-    projects.export_file= '111.json'
+    projects.export_file= 'projects.json'
     # Файл фикстуры
     projects.import_path=f'{settings.BASE_DIR}/projectsapp/fixtures/'
     projects.import_file='projects.json'
 
     projects.save()
     projects.update_db()
+
+@app.task(bind=True, ignore_result=True)
+def parser_adjust_task(self):
+    print('from settings_celery.py')
+    adjustes =  LoadAdjustes()
+    #Базовая директория
+    adjustes.worker_dir=settings.BASE_DIR
+    # Настройки БД
+    adjustes.dbname=pre_production.DATABASES['default']['NAME']
+    adjustes.user=pre_production.DATABASES['default']['USER']
+    adjustes.password=pre_production.DATABASES['default']['PASSWORD']
+    adjustes.host=pre_production.DATABASES['default']['HOST']
+    # Настройка файлов
+    # Файл выгрузки
+    adjustes.export_path = f'{settings.BASE_DIR}/projectsapp/data/'
+    adjustes.export_file= 'adjust.json'
+    # Файл фикстуры
+    adjustes.import_path=f'{settings.BASE_DIR}/projectsapp/fixtures/'
+    adjustes.import_file='adjust.json'
+
+    adjustes.save()
+    adjustes.update_db()
+
 
 @app.task(bind=True, ignore_result=True)
 def cleanup_unused_media_task():
