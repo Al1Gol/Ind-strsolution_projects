4156b1620ae8ae81bed6181e0cf3e3bddfe21131
Korepanov A <al1working@mail.ru>
2025-02-26 01:16:42 +0300

    Добавлены  задачи по добавлению согласований и проектов в планировщик Celery

--- backend/indsol_web/indsol_web/settings_celery.py
+++ backend/indsol_web/indsol_web/settings_celery.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 from django.conf import settings
 from celery import Celery
 from celery import shared_task
-from projectsapp.parsers import LoadProjects, LoadAdjustes
+from projectsapp.parsers import LoadProjects, LoadAdjustes, LoadAdjust, LoadProject
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indsol_web.settings.pre_production')
@@ -42,7 +42,7 @@ def parser_projects_task(self):
     projects.load()
 
 @app.task(bind=True, ignore_result=True)
-def parser_adjust_task(self):
+def parser_adjustes_task(self):
     print('from settings_celery.py')
     adjustes =  LoadAdjustes()
     #Базовая директория
@@ -61,6 +61,47 @@ def parser_adjust_task(self):
     adjustes.files = ['st_adjust.json', 'export_adjust.json']
 
     adjustes.load()
+
+@app.task(bind=True, ignore_result=True)
+def add_project_task(self, contract):
+    project =  LoadProject()
+    project.add_contract = contract
+    #Базовая директория
+    project.worker_dir=settings.BASE_DIR
+    # Настройки БД
+    project.dbname=pre_production.DATABASES['default']['NAME']
+    project.user=pre_production.DATABASES['default']['USER']
+    project.password=pre_production.DATABASES['default']['PASSWORD']
+    project.host=pre_production.DATABASES['default']['HOST']
+    # Путь файлов выгрузки
+    project.export_path = '/app/media/parse_data/'
+    # Путь файлов фикстур
+    project.import_path=f'{settings.BASE_DIR}/projectsapp/fixtures/'
+    # Файлы   
+    project.export_files = ['st_projects.json', 'export_projects.json']
+
+    project.load()
+
+@app.task(bind=True, ignore_result=True)
+def parser_adjust_task(self, contract):
+    adjust =  LoadAdjust()
+    adjust.add_contract = contract
+    #Базовая директория
+    adjust.worker_dir=settings.BASE_DIR
+    # Настройки БД
+    adjust.dbname=pre_production.DATABASES['default']['NAME']
+    adjust.user=pre_production.DATABASES['default']['USER']
+    adjust.password=pre_production.DATABASES['default']['PASSWORD']
+    adjust.host=pre_production.DATABASES['default']['HOST']
+    # Настройка файлов
+    # Путь выгрузки
+    adjust.export_path = '/app/media/parse_data/'
+    # Путь фикстуры
+    adjust.import_path=f'{settings.BASE_DIR}/projectsapp/fixtures/'
+    # Файлы
+    adjust.export_files = ['st_adjust.json', 'export_adjust.json']
+
+    adjust.load()
 
 
 @app.task(bind=True, ignore_result=True)
