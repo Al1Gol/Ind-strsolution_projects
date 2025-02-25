8dc868e398e6e99051a42970279da72c24775284
Korepanov A <al1working@mail.ru>
2025-02-26 00:31:31 +0300

    Добавлен парсер единичного проекта

--- backend/indsol_web/projectsapp/parsers.py
+++ backend/indsol_web/projectsapp/parsers.py
@@ -57,14 +57,15 @@ class LoadProjects():
 
                 # Проходим по списку договоров
                 for project in projects_dict:
+                    print(project)
                     contract = project['Номер']
                     name = project['Наименование'] if project['Наименование'] else None
                     start_date = project['ДатаНачала'] if project['ДатаНачала'] else None
                     deadline = project['СрокВыполнения'] if project['СрокВыполнения'] else None
                     is_completed = project['Выполнено']
                     actual_date = project['ДатаФактическогоВыполнения'] if project['ДатаФактическогоВыполнения'] else None
-                    resp = ''#project['Ответственный']
-                    resp_rp = ''#project['ОтветственныйРП']
+                    resp = project['Ответственный'] if project['Ответственный'] else None
+                    resp_rp = project['ОтветственныйРП'] if project['ОтветственныйРП'] else None
                     if contract in records:
                         cursor.execute('SELECT id FROM projectsapp_contracts where contract_number=%s', (contract, ))
                         id = cursor.fetchone()[0]
@@ -74,7 +75,6 @@ class LoadProjects():
 
     def save_file(self, fixture, file):
         # Сохранение фикстуры
-        print(f'{self.import_path}{file}')
         with open(f'{self.import_path}{file}', mode='w', encoding='utf-8') as file:
             json.dump(fixture, file, ensure_ascii=False, indent=4)
 
@@ -152,7 +152,86 @@ class LoadAdjustes():
     
     def save(self, fixture, file):
         # Сохранение фикстуры
-        print(f'{self.import_path}{file}')
+        with open(f'{self.import_path}add_project.json', mode='w', encoding='utf-8') as file:
+            json.dump(fixture, file, ensure_ascii=False, indent=4)
+
+    def update_db(self, file):
+	# Загрузка полученной фикстуры в приложение
+        if os.name == 'nt':
+            os.system(f'python -Xutf8 ./backend/indsol_web/manage.py loaddata add_project.json --settings=indsol_web.settings.debug')
+        else:
+            os.system(f'python -Xutf8 {self.worker_dir}/manage.py loaddata add_project.json --settings=indsol_web.settings.pre_production')
+
+
+
+#from celery import shared_task
+# Импорт проектов
+class LoadProject():
+    def __init__(self):
+        # Основная  директория
+        self.worker_dir = None
+        # Файл выгрузки
+        self.export_path = None
+        # Файл фикстуры
+        self.import_path=None
+        # Параметры подключения к БД
+        self.dbname = None
+        self.user = None
+        self.password = None
+        self.host = None
+        self.add_contract = None
+        self.export_files = []
+
+    def get_dict(self, id, contract, name, start_date, deadline, is_completed, actual_date, resp, resp_rp):
+        return {
+            "model": "projectsapp.projects",
+            #"pk": id,
+            "fields": {
+                "contract_id": id,
+                "name": name,
+                "start_date":  start_date,
+                "deadline": deadline,
+                "is_completed": is_completed,
+                "actual_date": actual_date,
+                "responsible": resp,
+                "responsible_rp": resp_rp
+                }
+        }
+
+    #
+    def load(self):
+        conn = psycopg2.connect(dbname=self.dbname, user=self.user, 
+                                password=self.password, host=self.host)
+        cursor = conn.cursor()
+
+        for export_file in self.export_files:
+            # Переменная для записи в фикстуру
+            fixture = []
+
+            # Исходный файл состоит из списка словарей
+            with open(f'{self.export_path}{export_file}', mode='r', encoding='utf-8-sig') as file:
+                projects_dict = json.loads(file.read())
+                projects_dict = projects_dict.get('tab')
+
+                # Проходим по списку договоров
+                for project in projects_dict:
+                    contract = project['Номер']
+                    name = project['Наименование'] if project['Наименование'] else None
+                    start_date = project['ДатаНачала'] if project['ДатаНачала'] else None
+                    deadline = project['СрокВыполнения'] if project['СрокВыполнения'] else None
+                    is_completed = project['Выполнено']
+                    actual_date = project['ДатаФактическогоВыполнения'] if project['ДатаФактическогоВыполнения'] else None
+                    resp = project['Ответственный']
+                    resp_rp = ''#project['ОтветственныйРП']
+                    if contract == self.add_contract:
+                        cursor.execute('SELECT id FROM projectsapp_contracts where contract_number=%s', (contract, ))
+                        id = cursor.fetchone()[0]
+                        fixture.append(self.get_dict(id, contract, name, start_date, deadline, is_completed, actual_date, resp, resp_rp))
+            self.save_file(fixture, export_file)
+            self.update_db(export_file)
+
+    def save_file(self, fixture, file):
+        # Сохранение фикстуры
         with open(f'{self.import_path}{file}', mode='w', encoding='utf-8') as file:
             json.dump(fixture, file, ensure_ascii=False, indent=4)
 
@@ -161,39 +240,23 @@ class LoadAdjustes():
         if os.name == 'nt':
             os.system(f'python -Xutf8 ./backend/indsol_web/manage.py loaddata {file} --settings=indsol_web.settings.debug')
         else:
-            os.system(f'python -Xutf8 {self.worker_dir}/manage.py loaddata {file} --settings=indsol_web.settings.pre_production')
-
+	        os.system(f'python -Xutf8 {self.worker_dir}/manage.py loaddata {file} --settings=indsol_web.settings.pre_production')
 
 
 if __name__ == "__main__":
-    projects =  LoadProjects()
+    add_project = LoadProject()
+    add_project.add_contract = "000000004"
+    #Базовая директория
+
     # Настройки БД
-    projects.dbname='indsol_test'
-    projects.user='postgres'
-    projects.password='123'
-    projects.host='localhost'
+    add_project.dbname='indsol_test'
+    add_project.user='postgres'
+    add_project.password='123'
+    add_project.host='localhost'
     # Настройка файлов
     # Файл выгрузки
-    projects.import_path='./backend/indsol_web/projectsapp/fixtures/'
-    projects.export_path = './backend/media/parse_data/'
-    projects.files = ['st_projects.json', 'export_projects.json']
+    add_project.import_path='./backend/indsol_web/projectsapp/fixtures/'
+    add_project.export_path = './backend/media/parse_data/'
+    add_project.export_files = ['export_projects.json']
 
-    projects.load()
-
-
-    adjustes =  LoadAdjustes()
-    # Настройки БД
-    adjustes.dbname='indsol_test'
-    adjustes.user='postgres'
-    adjustes.password='123'
-    adjustes.host='localhost'
-    # Настройка файлов
-    # Путь выгрузки
-    adjustes.export_path = './backend/media/parse_data/'
-    # Путь фикстуры
-    adjustes.import_path='./backend/indsol_web/projectsapp/fixtures/'
-    # Список файлов
-    adjustes.files = ['st_adjust.json', 'export_adjust.json']
-
-    adjustes.load()
-
+    add_project.load()
