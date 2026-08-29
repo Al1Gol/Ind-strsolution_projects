456b03c4be99be979aa8f884cff3504472fd542e
Korepanov A <al1working@mail.ru>
2026-08-29 03:28:40 +0300

    Актуализирован фронт

---
+++ frontend_v2/build/assets/axios-4d6usjtH.js
@@ -0,0 +1,1 @@
+import{i as e}from"./useAuthStore-DtdgS6LG.js";var{Axios:t,AxiosError:n,CanceledError:r,isCancel:i,CancelToken:a,VERSION:o,all:s,Cancel:c,isAxiosError:l,spread:u,toFormData:d,AxiosHeaders:f,HttpStatusCode:p,formToJSON:m,getAdapter:h,mergeConfig:g,create:_}=e;export{n as t};
